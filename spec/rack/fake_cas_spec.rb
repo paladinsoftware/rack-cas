92e1efd33afd707558a269b2b6e552342141ae03
Michał Trojanowski <michal.trojanowski@brandwatch.com>
2024-01-16 18:40:54 +0100

    FakeCAS adjusted to better simulate real authentication flow

--- spec/rack/fake_cas_spec.rb
+++ spec/rack/fake_cas_spec.rb
@@ -13,21 +13,56 @@ describe Rack::FakeCAS do
   end
 
   describe 'auth required request' do
+    before do
+      described_class.class_variable_set('@@cas_session_present', false)
+    end
+
     subject { get '/private' }
-    its(:status) { should eql 200 }
-    its(:body) { should match /username/ }
-    its(:body) { should match /password/ }
+    it { should be_redirect }
+    its(:location) { should eql 'http://example.org/login?service=http://example.org/private' }
+    its(:headers) { }
   end
 
-  describe 'login page request' do
-    subject { get '/fake_cas_login' }
-    its(:status) { should eql 200 }
-    its(:body) { should match /username/ }
-    its(:body) { should match /password/ }
+  describe 'fake_cas_login request' do
+    before do
+      described_class.class_variable_set('@@cas_session_present', cas_session_present)
+      get '/login', username: 'janed0', service: 'http://example.org/private'
+    end
+
+    context 'without cas session' do
+      let(:cas_session_present){ false }
+
+      subject { last_response }
+      its(:status) { should eql 200 }
+      its(:body) { should match /username/ }
+      its(:body) { should match /password/ }
+
+      describe 'session' do
+        subject { last_request.session['cas'] }
+        it { should be_nil }
+      end
+    end
+
+    context 'with cas session' do
+      let(:cas_session_present){ true }
+
+      subject { last_response }
+      it { should be_redirect }
+      its(:location) { should eql 'http://example.org/private?ticket=some-value' }
+
+      describe 'session' do
+        subject { last_request.session['cas'] }
+        it { should be_nil }
+      end
+    end
+
   end
 
   describe 'login request' do
-    before { get '/login', username: 'janed0', service: 'http://example.org/private' }
+    before do
+      described_class.class_variable_set('@@cas_session_present', false)
+      get '/logged_in', username: 'janed0', service: 'http://example.org/private'
+    end
 
     subject { last_response }
     it { should be_redirect }
@@ -35,9 +70,26 @@ describe Rack::FakeCAS do
 
     describe 'session' do
       subject { last_request.session['cas'] }
+      it { should be_nil }
+    end
+
+    it 'should have cas_session_present class variable set to true' do
+      expect(described_class.class_variable_get('@@cas_session_present')).to be(true)
+      subject
+    end
+  end
+
+  describe 'ticket validation request' do
+    before { get '/private', ticket: 'some-value' }
+
+    subject { last_response }
+    it { should be_redirect }
+    its(:location) { should eql 'http://example.org/private' }
+    its(:headers) { }
+
+    describe 'session' do
+      subject { last_request.session['cas'] }
       it { should_not be_nil }
-      its(['user']) { should eql 'janed0' }
-      its(['extra_attributes']) { should eql({}) }
     end
   end
 
@@ -73,7 +125,10 @@ describe Rack::FakeCAS do
                         })
     end
 
-    before { get '/login', username: 'janed0', service: 'http://example.org/private' }
+    before do
+      described_class.class_variable_set('@@cas_session_present', true)
+      get '/private', username: 'janed0', ticket: 'some-value'
+    end
 
     describe 'session' do
       subject { last_request.session['cas'] }
